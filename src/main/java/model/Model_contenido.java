15d7a1082562c5b82d91a7e44e7b10c6fde426cd
AdrianRodriguezJorge <139442845+AdrianRodriguezJorge@users.noreply.github.com>
2024-09-16 23:02:59 +0000

    23.03

--- src/main/java/model/Model_contenido.java
+++
@@ -1,8 +0,0 @@
-
-package model;
-
-public class Model_contenido {
-    // En el paquete “model” aparecerán las entidades o clases persistentes las que permitirán realizar el mapeo de los atributos de 
-    // las clases con los componentes de interfaz de usuario. Las clases del modelo tendrán los atributos, que se corresponden con las 
-    // columnas de la tabla, con sus respectivos métodos get() y set().
-}
--- src/main/java/visual/Visual_contenido.java
+++
@@ -1,9 +0,0 @@
-package visual;
-
-public class Visual_contenido {
-    // El en paquete “visual” estarán todas las clases correspondientes a la interfaz de usuario.
-    // Los componentes visuales deben mapear con las clases del modelo.
-
-    //NOTA: Aclarar que desde la interfaz de usuario solamente se realizarán las llamadas de los métodos que están en los servicios,
-    // no se deben realizar consultas desde estas.
-}
